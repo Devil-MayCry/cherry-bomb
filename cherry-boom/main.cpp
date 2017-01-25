000da9ac71eb5844d069b75e5f03784f6384297e
12301064@bjtu.edu.cn <12301064@bjtu.edu.cn>
2017-01-25 16:56:33 +0800

    update

--- cherry-boom/main.cpp
+++ cherry-boom/main.cpp
@@ -10,23 +10,15 @@
 #include "include/drafter/drafter.h"
 #include "include/jsoncpp/json-forwards.h"
 #include "include/jsoncpp/json.h"
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+
 int main(int argc, const char * argv[]) {
     
-    const char* blueprint ="# Group Land Weather"
-    "地块气象实时数据、预测数据、历史数据相关接口，请注意，所以的接口时间默认为 UTM+8 时区"
-    "## 地块实时天气 [/weather/realtime_land_weather{?land_id,basic_temperature}]";
-    //    drafter_parse_options option={false};
-    //    drafter_result * result = NULL;
-    //     drafter_check_blueprint(blueprint, &result, option);
-    //        // Serialize the result to print the warnings/errors
-    //        drafter_serialize_options options;
-    //        options.sourcemap = false;
-    //        options.format = DRAFTER_SERIALIZE_YAML;
-    //      char* out = drafter_serialize(result, options);
-    //        printf("The blueprint produces warnings or errors:\n\n%s\n", out);
-    //        free(out);
-    //
-    //        drafter_free_result(result);
+    const char* blueprint ="# Group Land Weather\n"
+    "地块气象实时数据、预测数据、历史数据相关接口，请注意，所以的接口时间默认为 UTM+8 时区\n"
+    "## 地块实时天气 [/weather/realtime_land_weather{?land_id,basic_temperature}]\n";
     drafter_parse_options parseOptions = {false};
     drafter_serialize_options options;
     options.sourcemap = false;
@@ -38,6 +30,38 @@ int main(int argc, const char * argv[]) {
     Json::Value value;
     Json::Reader reader;
     reader.parse(res,value);
-    std::cout<<value["content"][0];
-    // std::cout<<(value.get("content", "null"));
+    std::cout<< value;
+    std::string GroupNameString =  value["content"][0]["content"][0]["meta"]["title"].asString();
+    const char *GroupName = GroupNameString.c_str();
+    char letter[100];
+    strcpy(letter,GroupName);
+
+    const char *d = " ";
+    char *p;
+    p = strtok(letter,d);
+    std::string result;
+    while(p)
+    {
+        result = result + std::string(p);
+        p=strtok(NULL,d);
+    }
+    const char * resultInFile;
+    resultInFile = result.c_str();
+    char  result2[222];
+     strcpy(result2, resultInFile);
+    result2[0] = tolower(result2[0]);
+    std::cout<<result2;
+    std::string controllerFileName = "/tmp/"+std::string(result2)+"Controller.ts";
+    std::string serviceFileName = "/tmp/"+std::string(result2)+"Service.ts";
+    std::string repositoryFileName = "/tmp/"+std::string(result2)+"Repository.ts";
+    std::ofstream out(controllerFileName);
+    std::ofstream out2(serviceFileName);
+    std::ofstream out3(repositoryFileName);
+//    if (out.is_open())
+//    {
+//        out << "This is a line.\n";
+//        out << "This is another line.\n";
+//        out.close();
+//    }
+//    return 0;
 }
